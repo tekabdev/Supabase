8c7bc7dd32ba6674a69b63cd9e3207ad1b326006
ala TAKTAK <ala.taktak@tekab.dev>
2023-10-24 09:02:18 +0000

    add router

--- supabase-vue-3/src/main.js
+++ supabase-vue-3/src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+Vue.use(VueRouter)
 createApp(App).mount('#app')
